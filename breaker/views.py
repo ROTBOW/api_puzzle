3431f647c431234ac716f7b647ef97cbd9203707
Josiah Leon <sjosiahleon@gmail.com>
2024-08-13 10:06:08 -0700

    implemented start for the attempt endpoint

--- breaker/views.py
+++ breaker/views.py
@@ -6,12 +6,13 @@ from django.http import JsonResponse
 from .models import Attempt, Completion
 from .utils.db_utils import expiration_check, clean_expired
 from .utils.problem_generator import gen_problem
-from .utils.utils import time_till_expires
+from .utils.utils import time_till_expires, create_hash
 
 
 # Create your views here.
 def ping(req) -> JsonResponse:
     amount_del = clean_expired()
+    
     return JsonResponse({"INFO": f"Cleased db, amount removed: {amount_del}"})
 
 
@@ -55,11 +56,40 @@ def restart(req, email) -> JsonResponse:
     except:
         return JsonResponse({"Error": "No attempt found with that email"})
 
+
 # _and_attempt challenge
 def attempt(req, email, ans) -> JsonResponse:
     try:
-        pass
+        apt = Attempt.objects.get(email__exact=email)
+        # need to also account for the attempt being expired
+        
+        if create_hash(ans) == apt.curr_ans:
+            apt.gate += 1
+            apt.expires = timezone.now() + timedelta(hours=1)
+            
+            # check if challenge is done (reached the last gate)
+            # send them a congrats and mark their time on the leaderboard
+            # will prob do this with a helper func
+            if apt.gate >= 3:
+                
+                return JsonResponse({})
+            
+            else:
+                # use this gate (the starting one) to gen a problem
+                hint, problem, hashed_ans = gen_problem(apt.gate)
+                apt.curr_ans = hashed_ans
+                apt.save()
+                
+                return JsonResponse({
+                    'curr_gate': apt.gate,
+                    'hint': hint,
+                    'problem_str': problem,
+                    'expires': time_till_expires(apt.expires)
+                })
+        
+        else:
+            # need to delete the apt when they submit a wrong ans
+            return JsonResponse({"Info": "incorrect ans, terminating attempt"})
+        
     except:
-        pass
-    
-    return JsonResponse({'params': [email, ans]})
+        return JsonResponse({"Info": "There is no attempt with that email, start a new attempt by calling gate/<email>"})
